ee5d6f8342f16741dc2402ab550d26e235e15218
Akash Dasgupta <akash.dasgupta.jobs@gmail.com>
2025-10-03 05:09:20 +0530

    Lesson 3

---
+++ goto.php
@@ -0,0 +1,20 @@
+<?php
+
+$x = 20;
+
+
+echo "Before <br/>";
+
+if($x == 20){
+    goto jump; // Transfers the control to 
+}
+
+
+$name = "Akash";
+echo $name;
+
+jump: // here
+
+echo "After Jump <br/>"
+
+?>
---
+++ stringOperators.php
@@ -0,0 +1,22 @@
+<?php
+
+// . Operator
+
+$str = "Hi How are you ";
+$str1 = "I am Akash";
+
+echo $str.$str1; 
+
+
+$str .= $str1;
+
+
+echo "</br>";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+
+echo $str;
+
+
+?>
